75cb9c8511a5e3a2f7b4a2dd55fbc423be7aa887
Arash <47273556+arashnrim@users.noreply.github.com>
2023-11-16 10:49:53 +0800

    Update theming

--- app/src/main/java/sg/edu/np/mad/simplywords/HomePageFragment.java
+++ app/src/main/java/sg/edu/np/mad/simplywords/HomePageFragment.java
@@ -11,7 +11,10 @@ import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
 
 public class HomePageFragment extends Fragment {
 
