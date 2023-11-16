7eb5d23aa7393f0a5a78905b8f3dfd3f3a9cee5b
ZeroTzu <116237261+ZeroTzu@users.noreply.github.com>
2023-11-16 11:35:44 +0800

    basic simplified modal view

--- app/src/main/java/sg/edu/np/mad/simplywords/SimplifyFragment.java
+++ app/src/main/java/sg/edu/np/mad/simplywords/SimplifyFragment.java
@@ -1,11 +1,18 @@
 package sg.edu.np.mad.simplywords;
 
+import android.content.Context;
+import android.media.MediaSync;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -17,6 +24,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import sg.edu.np.mad.simplywords.adapter.SummaryAdapter;
+import sg.edu.np.mad.simplywords.model.Summary;
 import sg.edu.np.mad.simplywords.viewmodel.SummaryViewModel;
 
 public class SimplifyFragment extends Fragment {
@@ -41,6 +49,57 @@ public class SimplifyFragment extends Fragment {
         mSummaryViewModel.getAllSummaries().observe(getViewLifecycleOwner(), summaries -> {
             Log.d("HistoryFragment", "Summaries size: " + summaries.size());
             adapter.submitList(summaries);
+        });
+
+        adapter.setOnClickListener(new SummaryAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position, Summary model) {
+
+
+                // inflate the layout of the popup window
+                LayoutInflater inflater = (LayoutInflater)
+                        requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.floating_full_summary, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                TextView textView=popupWindow.getContentView().findViewById(R.id.floating_summary_MainTextView);
+                textView.setText(model.getOriginalText());
+
+                //change the text to the original when the original button is pressed.
+                popupWindow.getContentView().findViewById(R.id.floating_summary_OriginalButton).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        TextView textView=popupWindow.getContentView().findViewById(R.id.floating_summary_MainTextView);
+                        textView.setText(model.getOriginalText());
+                    }
+                });
+                //change the text to the simplified when the simplified button is pressed.
+                popupWindow.getContentView().findViewById(R.id.floating_summary_SimplifyButton).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        TextView textView=popupWindow.getContentView().findViewById(R.id.floating_summary_MainTextView);
+                        textView.setText(model.getSummarizedText());
+                    }
+                });
+
+                // dismiss the popup window when touched
+                popupWindow.getContentView().findViewById(R.id.floating_summary_ExitImageView).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+            }
         });
         return view;
     }
--- app/src/main/java/sg/edu/np/mad/simplywords/adapter/SummaryAdapter.java
+++ app/src/main/java/sg/edu/np/mad/simplywords/adapter/SummaryAdapter.java
@@ -1,6 +1,8 @@
 package sg.edu.np.mad.simplywords.adapter;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
@@ -10,6 +12,8 @@ import sg.edu.np.mad.simplywords.model.Summary;
 
 public class SummaryAdapter extends ListAdapter<Summary, SummaryViewHolder> {
 
+
+    private OnClickListener onClickListener;
     @NonNull
     @Override
     public SummaryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -20,6 +24,13 @@ public class SummaryAdapter extends ListAdapter<Summary, SummaryViewHolder> {
     public void onBindViewHolder(@NonNull SummaryViewHolder holder, int position) {
         Summary summary = getItem(position);
         holder.bind(summary.getCreatedAt(), summary.getOriginalText(), summary.getSummarizedText());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onClickListener != null) {
+                    onClickListener.onClick(position, summary );
+                }            }
+        });
     }
 
     public SummaryAdapter(@NonNull DiffUtil.ItemCallback<Summary> callback) {
@@ -36,5 +47,14 @@ public class SummaryAdapter extends ListAdapter<Summary, SummaryViewHolder> {
         public boolean areContentsTheSame(@NonNull Summary oldItem, @NonNull Summary newItem) {
             return oldItem.getOriginalText().equals(newItem.getOriginalText());
         }
+    }
+
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public interface OnClickListener {
+        void onClick(int position, Summary model);
     }
 }
