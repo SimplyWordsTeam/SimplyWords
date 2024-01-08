b38ce73615b24d4155de3412611af8b95575b0c3
Arash <47273556+arashnrim@users.noreply.github.com>
2024-01-08 19:35:09 +0800

    Implement Material You's Dynamic Colors feature

---
+++ app/src/main/java/sg/edu/np/mad/simplywords/SimplyWordsApplication.java
@@ -0,0 +1,15 @@
+package sg.edu.np.mad.simplywords;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class SimplyWordsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Applies Material You's Dynamic Color if applicable.
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
